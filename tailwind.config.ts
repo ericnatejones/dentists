972833c2e6e0144196a126826717bc80073ed139
Eric Jones <ericnatejones@gmail.com>
2024-08-16 15:18:28 -0600

    fixed tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,23 +16,22 @@ const config: Config = {
     },
   },
   plugins: [],
-  options: {
-    safelist: [
-      'grid-cols-1',
-      'grid-cols-2',
-      'grid-cols-3',
-      'grid-cols-4',
-      'grid-cols-5',
-      'grid-cols-6',
-      'grid-cols-7',
-      'grid-cols-8',
-      'grid-cols-9',
-      'grid-cols-10',
-      'grid-cols-11',
-      'grid-cols-12',
-      
-      // Add more if needed
-    ],
-  },
+  safelist: [
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+    'grid-cols-10',
+    'grid-cols-11',
+    'grid-cols-12',
+    
+    // Add more if needed
+  ],
+  
 };
 export default config;
